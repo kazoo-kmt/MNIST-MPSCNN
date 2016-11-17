3e2733e80f8ece5b2c4db589daaa4d3124d2004b
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2016-11-17 15:10:32 -0800

    From Apple WWDC's sample code & other sample codes

---
+++ mnist_MPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mnist_MPS
+//
+//  Created by 小林和宏 on 11/8/16.
+//  Copyright © 2016 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
